07946305c3aa07b1ea47fe6a91ec45c8f5c069f3
Brian Wong <bwong92@gmail.com>
2019-08-15 15:59:35 -0400

    SQLbears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 10, "male", "black", "angry", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",2,"male","black","angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",13,"male","black", "angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",20,"male","black", "angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",10,"male","black","angry",2);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",10,"male","black","angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",6,"male","black","angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",6,"male","black","angry",1);
